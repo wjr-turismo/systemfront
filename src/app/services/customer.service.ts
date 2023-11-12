24efcb0382ffb70068c19ed459c6e7552b10257a
maxfideles <maxfideles24@gmail.com>
2023-11-12 00:53:59 -0300

    Get Customers Method

--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { environment } from 'src/environments/environment';
+import { customerData } from '../models/customerData'
+import { Observable, catchError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
 
-  constructor() { }
+  baseUrl!:string
+  customers!: customerData[]
+  customer!: customerData
+
+
+  constructor(private http:HttpClient) {
+      this.baseUrl = environment.baseUrl
+   }
+
+   getCustomers():Observable<customerData[]>{
+
+      return this.http.get<customerData[]>(`${this.baseUrl}/customer`).pipe(
+        catchError((err:any , caught: Observable<customerData[]>) => {
+          console.log(err)
+          return caught
+        }
+
+      ))
+
+   }
+
+
 }
