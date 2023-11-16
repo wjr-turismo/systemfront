2d76c7270577147aa632f29049568fa27f4e0a3e
maxfideles <maxfideles24@gmail.com>
2023-11-16 09:12:34 -0300

    Roouting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
+import {RouterModule } from '@angular/router'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +15,11 @@ import { CustomerComponent } from './pages/customer/customer.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path:'',component: CustomerComponent}
+    ])
+
   ],
   providers: [],
   bootstrap: [AppComponent]
