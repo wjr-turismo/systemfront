80396ca5b88e78788eee204fa3364cbb67f37721
maxfideles <maxfideles24@gmail.com>
2023-11-17 08:35:43 -0300

    Form Group

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl('')
+
+  }) 
 
   constructor() { }
 
