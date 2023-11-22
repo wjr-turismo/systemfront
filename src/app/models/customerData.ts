7068b58779a59fac04b0344a7f9530f9ab984d63
maxfideles <maxfideles24@gmail.com>
2023-11-22 16:33:19 -0300

    Updated

--- src/app/models/customerData.ts
+++ src/app/models/customerData.ts
@@ -1,23 +1,23 @@
 export interface customerData {
-    "name": string,
-    "cpf": number,
-    "rg": string,
-    "birth": Date,
-    "email": string,
-    "places": string,
-    "tripDates": string,
-    "dependents": [
+    name: string,
+    cpf: number,
+    rg: string,
+    birth: Date,
+    email: string,
+    places: string,
+    tripDates: string,
+    dependents: [
       {
-        "name": string,
-        "description": string,
-        "rg": string,
-        "cpf": number
+        name: string,
+        description: string,
+        rg: string,
+        cpf: number
       }
     ],
-    "phone": [
+    phone: [
       {
-        "whats": string,
-        "personal": string
+        whats: string,
+        personal: string
       }
     ]
   }
