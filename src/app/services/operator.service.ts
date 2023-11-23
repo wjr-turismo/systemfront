d28109cfb5d4993db462d9e5dd19f418041dcbc2
maxfideles <maxfideles24@gmail.com>
2023-11-22 22:35:32 -0300

    Get Operatos Method

--- src/app/services/operator.service.ts
+++ src/app/services/operator.service.ts
@@ -1,11 +1,36 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, catchError } from 'rxjs';
+import { operatorData } from '../models/operatorData';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OperatorService {
 
-  constructor(private http: HttpClient) { }
-  
+  operators!: operatorData | any
+  baseUrl!: string
+
+  constructor(private http: HttpClient) { 
+
+    this.baseUrl = environment.baseUrl
+
+  }
+
+  getOperators():Observable<operatorData>{
+
+    const headers = { 'Authorization': `Bearer ${localStorage.getItem('token')}` }
+
+    this.operators = this.http.get<operatorData>(`${this.baseUrl}/operator`,{headers}).pipe(
+      catchError((err:any,caught:Observable<operatorData>) => {
+          console.log(err)
+          return caught
+      })
+    )
+        return this.operators
+  }
+
+
+
 }
