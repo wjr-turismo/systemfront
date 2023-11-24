712eebc54f6d6ae7f6edd5e75b2117ccdc122769
maxfideles <maxfideles24@gmail.com>
2023-11-24 13:27:51 -0300

    FormGroup and Register Sell Method

--- src/app/pages/sell/add-sell/add-sell.component.ts
+++ src/app/pages/sell/add-sell/add-sell.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { SellData } from 'src/app/models/sellData';
+import { SellService } from 'src/app/services/sell.service';
 
 @Component({
   selector: 'app-add-sell',
@@ -6,10 +9,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-sell.component.css']
 })
 export class AddSellComponent implements OnInit {
+  sellForm = new FormGroup({
+    operator: new FormControl(''),
+    bookingNumber: new FormControl(''),
+    sellAmount: new FormControl(null,Validators.required),
+    rav: new FormControl(null,Validators.required),
+    overBonus: new FormControl(null)
 
-  constructor() { }
+  })
+
+  sell: SellData | any
+
+
+  constructor(private service: SellService) { }
 
   ngOnInit(): void {
+  }
+
+
+  registersell(){
+    
+    this.sell = {
+      employeeName: localStorage.getItem('name'),
+      employeeEmail: localStorage.getItem('email'),
+      operator: this.sellForm.controls.operator.value,
+      bookingNumber: this.sellForm.controls.bookingNumber.value,
+      sellAmount: this.sellForm.controls.sellAmount.value,
+      rav: this.sellForm.controls.rav.value,
+      commission: null,
+      overBonus: this.sellForm.controls.overBonus.value,
+      sellerBonus: null,
+      managerBonus: null
+    }
+    console.log(this.sellForm)
+    console.log(`Sell: ${this.sell}`)
+
+    this.service.addSell(this.sell).subscribe((response) => {
+      console.log(response)
+    })
+
+
+
   }
 
 }
