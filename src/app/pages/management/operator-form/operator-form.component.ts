9e9ff91db88d40556205779be11a1eac3cc6844c
maxfideles <maxfideles24@gmail.com>
2023-11-23 21:11:09 -0300

    Form Created

--- src/app/pages/management/operator-form/operator-form.component.ts
+++ src/app/pages/management/operator-form/operator-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-operator-form',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OperatorFormComponent implements OnInit {
 
+  operatorForm= new FormGroup({
+    name :  new FormControl('',[Validators.required]),
+    companyName: new FormControl(''),
+    representativeName: new FormControl(''),
+    representativeEmail: new FormControl(''),
+    representativePhone: new FormControl('')
+  })
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  registeroperator(){
+    
   }
 
 }
