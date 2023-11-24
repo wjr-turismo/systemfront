70391b7b6795eeadf2e008fe5e6ee01a841fe13c
maxfideles <maxfideles24@gmail.com>
2023-11-23 21:33:57 -0300

    Register Operator Method

--- src/app/pages/management/operator-form/operator-form.component.ts
+++ src/app/pages/management/operator-form/operator-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators} from '@angular/forms';
+import { operatorData } from 'src/app/models/operatorData';
+import { OperatorService } from 'src/app/services/operator.service';
 
 @Component({
   selector: 'app-operator-form',
@@ -7,6 +9,8 @@ import { FormGroup, FormControl, Validators} from '@angular/forms';
   styleUrls: ['./operator-form.component.css']
 })
 export class OperatorFormComponent implements OnInit {
+
+  operator!: operatorData |any
 
   operatorForm= new FormGroup({
     name :  new FormControl('',[Validators.required]),
@@ -16,14 +20,28 @@ export class OperatorFormComponent implements OnInit {
     representativePhone: new FormControl('')
   })
 
-  constructor() { }
+  constructor(private service: OperatorService) { }
 
   ngOnInit(): void {
   }
 
 
   registeroperator(){
-    
+
+    this.operator = {
+      name: this.operatorForm.controls.name.value,
+      companyName: this.operatorForm.controls.companyName.value,
+      representativeName: this.operatorForm.controls.representativeName.value,
+      representativeEmail: this.operatorForm.controls.representativeEmail.value,
+      representativePhone: this.operatorForm.controls.representativePhone.value
+    }
+
+    console.log(this.operator)
+    this.service.addOperator(this.operator).subscribe((response) =>{
+        console.log(`RESPONSE: ${response}`)
+    })
+
+
   }
 
 }
