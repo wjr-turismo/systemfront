33b8ada7a7e3d4bdfb72b2fa957a1fbf310282d0
maxfideles <maxfideles24@gmail.com>
2023-11-23 23:15:43 -0300

    AddSell and GetSells Methods

---
+++ src/app/services/sell.service.ts
@@ -0,0 +1,44 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { SellData } from '../models/sellData';
+import { Observable, catchError } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SellService {
+
+  sells!: SellData[] | any
+  baseurl!: string
+
+  constructor(private http: HttpClient) { 
+    this.baseurl = environment.baseUrl
+  }
+
+
+  getSells():Observable<SellData>{
+
+    const headers = {'Authorization': `Bearer ${localStorage.getItem('token')}`}
+    this.sells = this.http.get<SellData>(`${this.baseurl}/sell`,{headers}).pipe(
+      catchError((err:any,caught:Observable<SellData>) => {
+        console.log(err)
+        return caught
+    })
+    )
+       return this.sells
+  }
+
+  addSell(sell:SellData):Observable<SellData>{
+    const headers = {'Authorization': `Bearer ${localStorage.getItem('token')}`}
+    this.sells = this.http.post<SellData>(`${this.baseurl}/sell`,sell,{headers}).pipe(
+      catchError((err:any,caught:Observable<SellData>) => {
+        console.log(err)
+        return caught
+    })
+    )
+       return this.sells
+  }
+
+
+}
