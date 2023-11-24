ba669b5fd0288e394c3799fa57fac96ffc4b814e
maxfideles <maxfideles24@gmail.com>
2023-11-24 00:44:25 -0300

    Get Sells Method

--- src/app/pages/sell/sells/sells.component.ts
+++ src/app/pages/sell/sells/sells.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SellData } from 'src/app/models/sellData';
+import { SellService } from 'src/app/services/sell.service';
 
 @Component({
   selector: 'app-sells',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SellsComponent implements OnInit {
 
-  constructor() { }
+  sells!: SellData[] | any
+
+  totalSells:number = 0
+  totalRAV:number = 0
+  totalCommission:number = 0
+
+  constructor(private service: SellService) { }
 
   ngOnInit(): void {
   }
+
+
+  getSells(){
+
+    this.service.getSells().subscribe((response) => {
+
+      console.log(`RESPONSE: ${response}`)
+
+      this.sells = response
+
+      for (let i = 0; i < response.length; i++) {
+        this.totalSells += response[i].sellAmount;
+        this.totalRAV += response[i].rav;
+        this.totalCommission += response[i].commission;
+        
+      }
+    
+
+    })
+
+  }
+
 
 }
