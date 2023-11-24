d213a5ce39f90281b352730b5ae290c7053a6095
maxfideles <maxfideles24@gmail.com>
2023-11-23 22:58:56 -0300

    Created

---
+++ src/app/models/sellData.ts
@@ -0,0 +1,12 @@
+export interface SellData {
+    employeeName: string,
+    employeeEmail: string,
+    operator: string,
+    bookingNumber: string,
+    sellAmount: number,
+    rav: number,
+    commission: number,
+    overBonus: number,
+    sellerBonus: number,
+    managerBonus: number
+}
