2dfe3150bfe318be0c685f039b013d9db99e096a
maxfideles <maxfideles24@gmail.com>
2023-12-07 10:28:25 -0300

    Component Created

--- src/app/pages/management/customer/customer.component.ts
+++ src/app/pages/management/customer/customer.component.ts
@@ -3,6 +3,7 @@ import { customerData } from '../../../models/customerData'
 import { CustomerService } from '../../../services/customer.service'
 import { environment } from 'src/environments/environment';
 import { AuthGuardService } from 'src/app/services/auth-guard.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-customer',
@@ -10,19 +11,31 @@ import { AuthGuardService } from 'src/app/services/auth-guard.service';
   styleUrls: ['./customer.component.css']
 })
 export class CustomerComponent implements OnInit {
+  customerForm = new FormGroup({
+    name :  new FormControl('',[Validators.required]),
+    cpf: new FormControl(null,[Validators.required,Validators.maxLength(11)]),
+    rg :  new FormControl(''),
+    birth :  new FormControl('') ,
+    email :  new FormControl('', [Validators.required, Validators.email]),
+    places: new FormControl(''),
+    tripDates: new FormControl(''),
+    whats :  new FormControl('',Validators.required) ,
+    personal :  new FormControl(''),
+    namedep1: new FormControl(''),
+    descriptiondep1: new FormControl(''),
+    rgdep1: new FormControl(''),
+    cpfdep1: new FormControl(null)
+  })
+
+  customer!: customerData|any
 
   customers!: customerData[]
-  customer!: customerData
-
-
-  user = {name:localStorage.getItem('user'),role:localStorage.getItem('role')}
-
-
- 
+  
+  user = {name:localStorage.getItem('user'),role:localStorage.getItem('role'),email:localStorage.getItem('email')}
 
   sct!:string
 
-  constructor(private service: CustomerService, private guard:AuthGuardService) {this.sct = environment.sct }
+  constructor(private service: CustomerService) {}
 
   ngOnInit(): void {
 
@@ -30,6 +43,44 @@ export class CustomerComponent implements OnInit {
 
   }
 
+  registercustomer(){
+    console.log(this.customerForm)
+
+    this.customer = {
+      name: this.customerForm.controls.name.value,
+      cpf: this.customerForm.controls.cpf.value,
+      rg: this.customerForm.controls.rg.value,
+      birth: this.customerForm.controls.birth.value,
+      email: this.customerForm.controls.email.value,
+      places: this.customerForm.controls.places.value,
+      tripDates: this.customerForm.controls.tripDates.value,
+      dependents: [
+        {
+          name: this.customerForm.controls.namedep1.value,
+          description: this.customerForm.controls.descriptiondep1.value,
+          rg: this.customerForm.controls.rgdep1.value,
+          cpf: this.customerForm.controls.cpfdep1.value
+        }
+      ],
+      phone: [
+        {
+          whats: this.customerForm.controls.whats.value,
+          personal: this.customerForm.controls.personal.value
+        }
+      ]
+    }
+
+    console.log(` CUSTOMER:  ${this.customer}`)
+    console.log(`${this.customerForm.controls.name.value}`)
+
+    this.service.addCustomer(this.customer).subscribe((response) => {
+      console.log(`Response: ${response}`)
+      this.customer = response
+    })
+    console.log(`RETURN: ${this.customer}`)
+
+    this.customerForm.reset()
+  }
 
 
   /*getCustomers(){
