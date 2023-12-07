7b6fb1120c5acb0c9543c8facb8ca07925efacb6
maxfideles <maxfideles24@gmail.com>
2023-12-07 10:29:23 -0300

    Redirection to Customer Component

--- src/app/pages/management/customer-list/customer-list.component.ts
+++ src/app/pages/management/customer-list/customer-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { customerData } from 'src/app/models/customerData';
 import { CustomerService } from 'src/app/services/customer.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-customer-list',
@@ -42,6 +43,11 @@ export class CustomerListComponent implements OnInit {
     }else{
       this.buttonText = "Adicionar Cliente"
     }
+  }
+
+  print(a:any){
+    console.log(a)
+    environment.idAux  = a
   }
 
 
