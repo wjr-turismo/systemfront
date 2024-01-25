b6681744e5df3329c55113f110ff534f10145690
maxfideles <maxfideles24@gmail.com>
2024-01-24 23:57:23 -0300

    Input Variables

--- src/app/pages/home/pac-card/pac-card.component.ts
+++ src/app/pages/home/pac-card/pac-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pac-card',
@@ -6,6 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pac-card.component.css']
 })
 export class PacCardComponent implements OnInit {
+
+  @Input() srcImg!:string
+  @Input() city!:string
+  @Input() pais!:string
+  @Input() price!:string
+  @Input() dias!:string
+  @Input() noites!:string
+  @Input() places!:string
+  @Input() atv!:string
 
   constructor() { }
 
