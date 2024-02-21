a1f37c270f4bcd8956eec05d325985716c9b2792
maxfideles <maxfideles24@gmail.com>
2024-02-21 02:31:13 -0300

    BaseUrl updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://wjr-back-production.up.railway.app/pacs',
+  baseUrl: 'https://wjr-back-production.up.railway.app',
   sct: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9',
   idAux: 0,
   email:'',
